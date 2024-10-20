a01d3ee1702a7d9471ee74e814d060b0286aea8e
sourabh151 <jhamnanisourabh2@gmail.com>
2024-10-21 01:17:31 +0530

    starting express course

--- node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+console.log('Express Tutorial');
+const express = require("express");
+const app = express();
+app.use(express.static('./navbar-app'))
+
+app.get('/',(req,res)=>{
+    res.status(200).send('hello brotha');
+})
+app.all('*',(req,res)=>{
+    res.status(404).send('resource not found');
+})
+app.listen(5000,()=>{
+    console.log("server is listening on port 5000");
+})
