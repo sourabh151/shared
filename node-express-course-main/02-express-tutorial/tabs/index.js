21e15c99ab4c7080a57f85165c22e83fd6c2e5d6
sourabh jhamnani <jhamnanisourabh2@gmail.com>
2024-10-24 10:40:53 +0530

    ”mobile”

--- node-express-course-main/02-express-tutorial/tabs/index.js
+++ node-express-course-main/02-express-tutorial/tabs/index.js
@@ -1,12 +1,14 @@
 async function start() {
-  const response = await fetch("http://localhost:5000/api/products");
-  const products = await response.json();
-//   console.log(products);
-  const list = products.map((product) => {
-    return `<li class = "item item-${product.id}">${product.name}</li>`;
-  });
-  console.log(list);
-  
-  document.getElementsByClassName("list")[0].innerHTML = list.join('');
+    const response = await fetch("http://localhost:5000/api/products");
+    const products = await response.json();
+    //   console.log(products);
+    const list = products.map(product => {
+        return `<li class = "item item-${product.id}"><a href = "/">${product.name}</a></li>`;
+    });
+    const links = document.querySelector("li");
+    console.log(links);
+    //console.log(list);
+
+    document.getElementsByClassName("list")[0].innerHTML = list.join("");
 }
 start();
