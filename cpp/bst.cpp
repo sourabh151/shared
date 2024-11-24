d59d11073c72e102c1ffcdeb7d51a7655d488a43
sourabh jhamnani <jhamnanisourabh2@gmail.com>
2024-11-24 20:00:05 +0530

    added delete function in bst

--- cpp/bst.cpp
+++ cpp/bst.cpp
@@ -6,10 +6,11 @@ struct Node{
 	int data;
 	Node* left;
 	Node* right;
-}
+};
 
-Node* Insert(root,data){
-	Node* node = new Node(data);
+Node* Insert(Node* root,int data){
+	Node* node = new Node();
+	node->data = data;
 	node->left = node->right = NULL;
 	if(root == NULL){
 		return node;
@@ -23,15 +24,28 @@ Node* Insert(root,data){
 	return root;
 }
 
-void Print(root){
+void Print(Node* root){
 	if(root == NULL){
 		return;
 	}
 	Print(root->left);
-	cout<<root->data + " ";
+	cout<<root->data<<endl;
+	Print(root->right);
 	
+}
+int Delete(Node* root,data){
+	if(root == NULL){
+		return 0;
+	}
+	if(root->data = )
 }
 
 int main(){
-	cout<<"hello world"<<endl;
+	Node* root = NULL;
+	root = Insert(root,6);
+	root = Insert(root,10);
+	root = Insert(root,5);
+	root = Insert(root,7);
+	// cout<<root->right->left->right->data<<endl;
+	Print(root);
 }
