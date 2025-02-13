c3f10bc670a744f3036796cb2810146cd738bda2
sourabh jhamnani <jhamnanisourabh2@gmail.com>
2025-02-13 10:44:58 +0530

    c f m

---
+++ leetcode/maxSumOfAPair.js
@@ -0,0 +1,7 @@
+function maxSum(nums){
+  let c = new Map();
+  for(let i = 0;i<nums.length;i++){
+
+  }
+}
+maxSum([18,43,36,13,7])
--- node-express-course-main/06-jobs-api/starter/controllers/jobs.js
+++ node-express-course-main/06-jobs-api/starter/controllers/jobs.js
@@ -5,41 +5,41 @@ const { BadRequestError } = require("../errors");
 const ObjectId = require("mongoose").Types.ObjectId;
 
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy: req.createdBy });
-    res.status(200).json({ jobs, count: jobs.length });
+  const jobs = await Job.find({ createdBy: req.createdBy });
+  res.status(200).json({ jobs, count: jobs.length });
 };
 const postJob = async (req, res) => {
-    const { company, position } = req.body;
-    Job.create({ company, position, createdBy: req.createdBy })
-    res.status(StatusCodes.CREATED).json({ success: true })
+  const { company, position } = req.body;
+  Job.create({ company, position, createdBy: req.createdBy })
+  res.status(StatusCodes.CREATED).json({ success: true })
 }
 const deleteJob = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!id || !ObjectId.isValid(id)) {
-        throw new BadRequestError("please provide valid Object Id");
-    }
-    const result = await Job.findOneAndDelete({ _id: id, createdBy: req.createdBy })
+  if (!id || !ObjectId.isValid(id)) {
+    throw new BadRequestError("please provide valid Object Id");
+  }
+  const result = await Job.findOneAndDelete({ _id: id, createdBy: req.createdBy })
 
-    res.json({ success: true, result })
+  res.json({ success: true, result })
 }
 const updateJob = async (req, res) => {
-    const { id } = req.params;
-    if (!id || !ObjectId.isValid(id)) {
-        throw new BadRequestError("please provide valid Object Id");
+  const { id } = req.params;
+  if (!id || !ObjectId.isValid(id)) {
+    throw new BadRequestError("please provide valid Object Id");
+  }
+  const update = {};
+  for (k in req.body) {
+    if (req.body[k] != "") {
+      update[k] = req.body[k]
     }
-    const update = {};
-    for (k in req.body) {
-        if (req.body[k] != "") {
-            update[k] = req.body[k]
-        }
-    }
+  }
 
-    const result = await Job.findByIdAndUpdate({ _id: id }, { $set: update }, { new: true ,runValidators:true});
-    if(!result){
-        return res.status(StatusCodes.NOT_MODIFIED)
-    }
-    res.status(StatusCodes.OK).json({ success: true, result });
+  const result = await Job.findByIdAndUpdate({ _id: id }, { $set: update }, { new: true, runValidators: true });
+  if (!result) {
+    return res.status(StatusCodes.NOT_MODIFIED)
+  }
+  res.status(StatusCodes.OK).json({ success: true, result });
 }
 
 module.exports = { getAllJobs, postJob, deleteJob, updateJob };
