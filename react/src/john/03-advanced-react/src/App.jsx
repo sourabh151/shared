67998f89f9c154af284bf655eec7cdf3d70e9bb4
sourabh151 <jhamnanisourabh2@gmail.com>
2025-03-07 10:57:39 +0530

    cfpc

--- react/src/john/03-advanced-react/src/App.jsx
+++ react/src/john/03-advanced-react/src/App.jsx
@@ -1,12 +1,9 @@
 import './index.css';
 // import Final from "./tutorial/01-useState/final/02-useState-basics"
-<<<<<<< HEAD
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function"
+// import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function"
 // import Starter from "./tutorial/04-project-structure/starter/App.jsx"
+import Starter from "./tutorial/06-forms/starter/01-controlled-inputs"
 
-=======
-import Starter from "./tutorial/04-project-structure/startert/Temp.jsx"
->>>>>>> 28e492d84e22955d7fce044059a10c5cb0102a94
 function App() {
   return (
     <div className='container'>
--- react/src/john/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ react/src/john/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  function handleChange(e) {
+    if (e.target.id === "name") {
+      setName(e.target.value)
+    }
+    else if (e.target.id === "email") {
+      setEmail(e.target.value)
+    }
+  }
+  return <form className="form">
+    <h4>controlled inputs</h4>
+    <div className="form-row">
+      <label htmlFor="name" className="form-label">
+        name
+      </label>
+      <input type="text" className="form-input" id="name" value={name} onChange={handleChange} />
+    </div>
+    <div className="form-row">
+      <label htmlFor="email" className="form-label">
+        email
+      </label>
+      <input type="email" className="form-input" id="email" value={email} onChange={handleChange} />
+    </div>
+    <button type="submit" className="btn btn-block">submit</button>
+  </form>
+
 };
 export default ControlledInputs;
