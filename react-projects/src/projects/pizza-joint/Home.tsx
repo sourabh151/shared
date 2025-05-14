982be26b820c97492a20f27300d786913f1b86fc
sourabh151 <jhamnanisourabh2@gmail.com>
2025-05-14 07:06:45 +0530

    cfpc14/05

--- react-projects/src/projects/pizza-joint/Home.tsx
+++ react-projects/src/projects/pizza-joint/Home.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 import "./Home.css"
 function Home() {
   return (
-    <div className="w-screen h-screen Home">
+    <div className="w-screen min-h-screen Home">
       <Navbar />
       <Outlet />
     </div>
--- react-projects/src/projects/pizza-joint/HomePage.tsx
+++ react-projects/src/projects/pizza-joint/HomePage.tsx
@@ -1,11 +1,15 @@
+import { motion } from "framer-motion"
 import { Link } from "react-router-dom"
 
 function HomePage() {
   return (
-    <div className="w-full h-full flex flex-col justify-center gap-10 items-center">
+    <motion.div className="w-full h-full flex flex-col justify-center gap-10 items-center"
+    initial={{x:"-100vw"}}
+    animate={{x:0}}
+    >
       <p className="text-white text-2xl font-bold">Welcome to Pizza Joint</p>
       <Link to="/base" className="border border-white rounded-full text-slate-200 py-2 px-4">Create Your Pizza</Link>
-    </div>
+    </motion.div>
   )
 }
 
